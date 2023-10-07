5aea374dcc0efb39356c5cff1818bbb57140dfad
Edgars Lakis <38805016+edgarslakis@users.noreply.github.com>
2023-10-07 10:21:24 +0000

    Uzrakstīts risinājuma apraksts un vispārināta testešana ar nejauši ģenerētiem izmēriem i, j

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# bin-packing
-Short practical example in combinatorial optimisation
+# Kravas automašīnu aizpildīšana ar kastēm (bin packing). Mazais praktiskais darbs
+Edgars Laķis
+7.10.2023
+## Uzdevuma formulējums 
+Dotas m automašīnas, katra ar kravas kastes izmēru w[i] x l[i]. Dotas n kastes ar izmēriem p[j] x g[j]. 
+Salikt visas kastes pēc iespējas mazāk automašīnās. Paliekam 2D - kastes nedrīkst likt vienu otrai virsū.
+
+## Algoritma apraksts
+1. Konstruējam sākotnējo risinājumu ar First Fit Decreasing heiristiku. 
+1. Meklē lokālo minimumu ar Simulated Annealing algoritmu.
+1. Kamēr temperatūra nesasniedz pārtraukšanas vērtību (0.1):
+   - Izvēlamies jaunu risinājumu ar random seed - kaimiņus
+   - Meklē lokālo minimumu
+   - Ja atrastais optimums ir labāks par iepriekšējo, tad atceramies to
+1. Veicam L iterācijas iepriekšējam solim, katru reizi atiestato temperatūru
+1. Labāko atrasto risinājumu pasludinām par galīgo
+### Domēns
+Domēns sastāv no: 
+- Novērtēšanas un izmaksu funkcijas
+- Gājiena un apkārtnes funkcijas
+- Datu struktūrām, kas apraksta risinājumu:
+  - kravas mašīnu masīvs (bins) ar atsevišķām mašīnām (bin) 
+  - kravas kastu masīvs (items) ar atsevišķām kastēm (item)
+  - mašīnu piepildījuma struktūra apkopota masīvā bin_contents
+  - sakrautās kravas izmērs glabājas total_sizes
+Plānošanas mainīgo kopa šajā uzdevumā ir m kravas automašīnas un n kastes ar piekārtotu garumu un platumu. Galvenais ierobežojums meklēšanas telpā ir kravas mašīnas izmērs 2D, respektīvi, kravas mašīnā nevar ielikt vairāk kravu, kā to ļauj mašīnas izmērs 2D.
+### Novērtēšana
+Risinājums ir mašīnai piekārtotas kravas kastes. Risinājuma novērtēšana tiek veikta, saskaitot kravas kastu kopējo izmēru katrā mašīnā. Jo mazāk mašīnās var satilpināt visas kastes, jo optimālāks risinājums. Izmaksu funkcija atgriež piepildītākās kravas mašīnas aizpildījumu (max). Par labāku risinājumu tiek uzskatīts arī vienmērīgāk sadalītas kastes starp mašīnām, respektīvi, mazāk "pārbāztas" mašīnas.
+Tālāk aprēķina izvēlētā risinājuma izmaksas un starpību starp līdz šim labākā risinājuma izmaksām (delta). Ja labāks par redzeto (izmaksu starpība (delta) ir negatīva) VAI metot kauliņu. Varbūtība akceptēt sliktāku risinājumu (random < exp) zemā temperatūrā samazinās. Varbūtība samazinās arī, ja liela izmaksu starpība (delta).
+Beigās pasludinām jaunatrasto risnājumu par līdz šim labāko risinājumu, ja tā izmaksas ir mazākas par iepriekšējo.
+### Gājiens
+Gājienā izvēlas nejaušu kastu pāri (kaimiņus) no visu mašīnu masīva. Kaimiņu pāri var veidot kastes gan no vienas mašīnas, gan no dažādām mašīnām. Izvēlēto kastu pāri samaina vietām. Tālāk rēķina (novērtē) šo jauno kravu sadalījumu pa mašīnām. Nākamais gājiens sākas ar samazinātu temperatūras (ar noteiktu 5% soli).
+Šāds gājiens tiek atkārtots L reizes (iterācijas).
+## Testēšanas apraksts
+Testēt iespējams norādot automašīnu skaitu m un kravas kastes n.
+Sākotnēji testēju ar manuāli ievadītām kravas kastēm un mašīnām.
+### Izpildes laiks
+Optimizācijas algoritma izpilde prasa 3..5 milisekundes ar vienu iterāciju. Desmit iterācijām - 15..16 milisekundes.
+### Risinājuma kvalitāte
+Par optimumu varētu uzskatīt vienmērīg izkārtotu kravu pa iespējami maz mašīnām. Novēroju, ka minimālais mašīnu skaits tiek sasniegts jau ar First Fit Decreasing algoritmu. Simmulated Anealing uzlabo kravas izkārtojumu to vienmērīgojot.
--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-# Bin Packing uzdevums.
-# Uzdevuma formulējums
-# Dotas m automašīnas, katra ar kravas kastes izmēru w[i] x l[i].
-# Dotas n kastes ar izmēriem p[j] x g[j]. Salikt visas kastes pēc iespējas mazāk automašīnās.
-# Paliekam 2D - kastes nedrīkst likt vienu otrai virsū.
+# Edgars Lakis, 7.10.2023
+# Mazais praktiskais darbs - Bin Packing uzdevums.
 import random
 import math
 import time
 
 #Uzkonstruē un atrod sākotnējo risinājumu.
 def first_fit_decreasing(items, bins):
-    # Kastes (items) tiek sarindotas pēc to izmēriem (garums * platums), lai lielākās kastes sākumā
+    # Kastes (items) tiek sarindotas pēc to izmēriem (garums * platums), lai lielākās kastes ir masīva sākumā
     items.sort(key=lambda item: item[0] * item[1], reverse=True)
 
-    # Uzkonstruē TUKŠAS kravas
+    # Uzkonstruē TUKŠAS kravas mašīnas
     bin_count = len(bins)
+
+    # Risinājums apkopo visas mašīnas
     bin_contents = [[] for _ in range(bin_count)]
 
     # First Fit Decreasing algoritms pēc kārtas ņem kastes, sākot ar lielāko
     for item in items:
         # Kasti (item) mēģina ielikt kādā no pieejamajām kravām (bins[i]), ņemot vērā kravas izmēra ierobežojumu
         for i in range(bin_count):
-            # Pirmās iekavas - kravā jau esošo kastu summa. Otrās - tekošā kaste. Trešās - kravas izmēra ierobežojums
+            # Pirmās iekavas - mašīnā jau esošās kravas summa. Otrās - tekošā kaste. Trešās - kravas izmēra ierobežojums
             if (sum(w * l for w, l in bin_contents[i]) + (item[0] * item[1]) <= (bins[i][0] * bins[i][1])):
                 # Ja ir vieta tekošajai kastei, tad to pievieno i-tajā kravā
                 bin_contents[i].append(item)
@@ -34,7 +33,7 @@ def first_fit_decreasing(items, bins):
 def calculate_total_size(bin_contents):
     return [sum(w * l for w, l in bin) for bin in bin_contents]
 
-# Aprēķina kravas mašīnu kopējo aizpildījumu (izmaksas)
+# Izmaksu funkcijas vērtība ir tas, 
 def calculate_cost(bin_contents):
     total_sizes = calculate_total_size(bin_contents)
     return max(total_sizes)
@@ -47,37 +46,40 @@ def simulated_annealing(initial_solution, TEMPERATURE, T_STEP, bins):
     best_solution = current_solution
     best_cost = current_cost
 
-    while TEMPERATURE > 0.1: #CIKLS (skatīt komentāru cikla beigās). Apstāšanās kritērijs - temperatūra sasniedz 0.1
-        # 2. Atrast apkārtni. IZVĒLAS nākošā risinājuma kandidātu no apkārtnes nejauši mainot divas kastes vietām
-        neighbor_solution = current_solution.copy()
-        try:
-            bin1 = random.randint(0, len(neighbor_solution) - 1)
-            bin2 = random.randint(0, len(neighbor_solution) - 1)
-            item1 = random.randint(0, len(neighbor_solution[bin1]) - 1)
-            item2 = random.randint(0, len(neighbor_solution[bin2]) - 1)
-        except ValueError:
-            #print("paņema tukšu kasti")
-            continue
-        # Samaina vietām divas kastes:
-        neighbor_solution[bin1][item1], neighbor_solution[bin2][item2] = neighbor_solution[bin2][item2], neighbor_solution[bin1][item1]
-        
-        # Aprēķinām izvēlētā risinājuma izmaksas un starpību starp līdz šim labākā risinājuma izmaksām (delta)
-        neighbor_cost = calculate_cost(neighbor_solution)
-        delta_cost = neighbor_cost - current_cost
-        
-        # 3. Novērtē apkārtni. Ja labāks par redzeto (izmaksu starpība (delta) ir negatīva) VAI metot kauliņu - random.random().
-        # Varbūtība akceptēt sliktāku risinājumu (random < exp) zemā temperatūrā samazinās. Tāpat, ja liela izmaksu starpība.
-        if delta_cost < 0 or random.random() < math.exp(-delta_cost / TEMPERATURE):
-            current_solution = neighbor_solution
-            current_cost = neighbor_cost
-        # 4. Pasludinām jaunatrasto risnājumu par līdz šim labāko risinājumu, ja tā izmaksas ir mazākas.
-        if current_cost < best_cost:
-            best_solution = current_solution
-            best_cost = current_cost #Piefiksējām jaunatrasta risinājuma izmaksas
+    for _ in range(L):
+        T = TEMPERATURE
+        while T > 0.1: #CIKLS (skatīt komentāru par T cikla beigās). Apstāšanās kritērijs - temperatūra sasniedz 0.1
+            # 2. Atrast apkārtni - iegūst nejaušus kaimiņus. Tādā veidā izvēlas nākošā risinājuma kandidātu no apkārtnes nejauši mainot divas kastes vietām
+            neighbor_solution = current_solution.copy()
+            try:
+                bin1 = random.randint(0, len(neighbor_solution) - 1)
+                bin2 = random.randint(0, len(neighbor_solution) - 1)
+                item1 = random.randint(0, len(neighbor_solution[bin1]) - 1)
+                item2 = random.randint(0, len(neighbor_solution[bin2]) - 1)
+            except ValueError:
+                #print("paņema tukšu kasti")
+                continue
 
-        # 5. Izvēlas nākošo risinājumu, mainot TEMPERATŪRAS slieksni (lineāra mainīšana)
-        TEMPERATURE *= T_STEP
-        # Turpina CIKLU līdz izpildās apstāšanās kritērijs - temperatūra samazinās no 1 līdz 0.1 ar soli T_STEP (0.05)
+            # Samainam vietām divas kastes:
+            neighbor_solution[bin1][item1], neighbor_solution[bin2][item2] = neighbor_solution[bin2][item2], neighbor_solution[bin1][item1]
+            
+            # Aprēķinām izvēlētā risinājuma izmaksas un starpību starp līdz šim labākā risinājuma izmaksām (delta)
+            neighbor_cost = calculate_cost(neighbor_solution)
+            delta_cost = neighbor_cost - current_cost
+            
+            # 3. Novērtē apkārtni. Ja labāks par redzeto (izmaksu starpība (delta) ir negatīva) VAI labāks par metamā kauliņā random.
+            # Varbūtība akceptēt sliktāku risinājumu (random < exp) zemā temperatūrā samazinās. Tāpat, ja liela izmaksu starpība.
+            if delta_cost < 0 or random.random() < math.exp(-delta_cost / T):
+                current_solution = neighbor_solution
+                current_cost = neighbor_cost
+            # 4. Pasludinām jaunatrasto risnājumu par līdz šim labāko risinājumu, ja tā izmaksas ir mazākas.
+            if current_cost < best_cost:
+                best_solution = current_solution
+                best_cost = current_cost # Piefiksējām jaunatrasta risinājuma izmaksas
+
+            # 5. Izvēlas nākošo risinājumu, mainot TEMPERATŪRAS slieksni (lineāra mainīšana)
+            T *= T_STEP
+            # Turpina CIKLU līdz izpildās apstāšanās kritērijs - temperatūra samazinās no 1 līdz 0.1 ar soli T_STEP (0.05)
     return best_solution
 
 
@@ -90,27 +92,39 @@ def print_bins(bin_contents):
 if __name__ == "__main__":
     # Domēna definēšana, vienumi un to atkarības.
     # Šeit aprakstu datu struktūru problēmas risinājumam.
-    # Risinājums ir kravas kastei piekārtotas kastes. Kravas kastes ir plānošanas entītas - var mainīt saites uz kastēm. Kastes (item) aparaksta plānošanas vērtību kopu.
-    # Example input: a list of items with sizes (p[j], g[j]) and bins with sizes (w[i], l[i])
+    # Risinājums ir mašīnai piekārtotas kravas kastes. Kastes (items) aparaksta plānošanas vērtību kopu.
     # Testēšanas piemēri:
-    items = [(4, 1), (8, 2), (1, 3), (4, 2), (2, 1),
-             (7, 3), (4, 1), (8, 2), (1, 3)]
-    bins = [(10, 4), (12, 5), (12, 5)]
-    start = time.time()
-    # Solve the Bin Packing Problem using First Fit Decreasing
-    bin_contents = first_fit_decreasing(items, bins)
-    print(bin_contents)
+    # items = [(7, 2), (1, 3), (4, 2), (2, 1), (2, 1), (1, 3), 
+    #          (7, 3), (4, 1), (8, 2), (1, 3), (8, 2), (1, 3), (4, 2)]
+    # bins = [(4, 5), (4, 5), (4, 5), (4, 5), (4, 5), (4, 5)]
+    random.seed(9001)
+    items = []
+    bins = []
+    n = 12 # kravas kastu skaits
+    m = 5 # kravas mašīnu skaits
+    for _ in range(n):
+        i = random.randint(1,5)
+        items.append((i,i))
+    for _ in range(m):
+        j = random.randint(1,10) 
+        bins.append((j,j))
+    print("Items:", items)
+    print("Bins:", bins)
+
     # konstantes optimizācijai ar Simulated Annealing
     TEMPERATURE = 1000
     T_STEP = 0.95
-    # Saraksts ar iterāciju skaits katrai temperatūrai
-    L = [3,3]
+    # Iterāciju skaits
+    L = 1
+
+    start = time.time()
+    # Solve the Bin Packing Problem using First Fit Decreasing
+    bin_contents = first_fit_decreasing(items, bins)
+    print("Sākotnējais risinājums:", bin_contents)
+        
+    # Optimālā atrisinājuma instance
     optimized_solution = simulated_annealing(bin_contents, TEMPERATURE, T_STEP, bins)
     end = time.time()
     # Print the bins
     print_bins(optimized_solution)
     print(end-start)
-
-
-# Izmantotie informācijas avoti:
-# https://developers.google.com/optimization/pack/bin_packing
