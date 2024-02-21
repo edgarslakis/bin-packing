83c2c5408a2e17a1da129a4990abb17b3a66a817
Edgars Lakis <38805016+edgarslakis@users.noreply.github.com>
2024-02-21 08:40:30 +0000

    README clarified

--- README.md
+++ README.md
@@ -24,7 +24,28 @@ Domēns sastāv no:
   - mašīnu piepildījuma struktūra apkopota masīvā bin_contents
   - sakrautās kravas izmērs glabājas total_sizes
 Plānošanas mainīgo kopa šajā uzdevumā ir m kravas automašīnas un n kastes ar piekārtotu garumu un platumu. Galvenais ierobežojums meklēšanas telpā ir kravas mašīnas izmērs 2D, respektīvi, kravas mašīnā nevar ielikt vairāk kravu, kā to ļauj mašīnas izmērs 2D.
+### Arhitektūra
+Sākotnēji izmantoju noklusēto Solvera uzstādījumus (ekrānšāviņš). Vien precizējot risināšanas laiku (TerminationConfig().withUnimprovedSecondsSpentLimit) - 10 sekundes. Ar dzeltenu atzīmēts, Akceptors pēc noklusējuma izmanto LateAcceptance, bet Klejotājs (Forager) pasludina pirmo par uzvarētāju. 
+
+### Apkārtnes 
+funkciju norāda moveSelector, kurš pēc noklusējuma ir ListChange un ListSwap apvienojums. T.i. Change nodrošina, ka varam nonākt līdz jebkuram risinājumam meklēšanas telpā, un Swap dod paātrinājumu nonākšanai optimumā.
+ChangeMove var izvēlēties paciņām piekārtotās (entitySelector) mašīnas no saraksta List<Vehicle vehicleList>, kurš attiecīgi ir anotēts kā @ValueRangeProvider. SwapMove maina vietām paciņas (plānošanas objekti). Pamēģināju arī pillarChangeMoveSelector ar pilāru lielumu no 1 līdz 3, taču risinājums neizdevās sekojošas kļudas dēļ: The selector (DefaultPillarSelector(FromSolutionEntitySelector(Pack))) with randomSelection  (true) and sub pillars does not support getSize() because the number of sub pillars scales exponentially.
+
+
+Pats darbā pamēģināju pamainīt Solvera noklusēto konfigurāciju SolverConfig.xml failā. Piemērma, Akceptora algoritmu nomainīju no noklusētā Late_Acceptance uz entityTabuSize = 5 un klejotāja (forager) uz acceptedCountLimit = 5. Pamēģināju mainīt entityTabu size uz 3 un 2, tāpāt valueTabuSize. Termination laiku paildzinot desmitkārt nenovēroju risinājuma uzlabošanos. Taču manāms ieguvums bija palielinot klejotāja acceptedCountLimit no 5 uz 20.
+
+
+
+
 ### Novērtēšana
+Pēc benchmarking var spriest, ka labākais risinājums tiek atrasts ļoti agri un tas vairs neuzlabojas atlikušajā risināšanas laikā. Visticamāk risinājums ir tuvu optimumam. Labāku risinājumu varētu sasniegt ar citiem algoritmiem vai krasi mainot to parametrus.
+
+Vispirms veicu mērogošanas testu, palielinot problēmas mērogu sākot ar 5 paciņām un vienu mašīnu līdz 100 paciņām uz 20 mašīnām. Pie šāda entīšu skaita un piekārtoto atribūtu vērtībām Hardscore ir 0 visos mērogos. Soft_score pieaug līdz ar problēmas mērogu, jo vienkārši pieaug automašīnu skaits (Cost)
+
+Testēju arī veselu BluePrint paneli ar dažādiem Local Search algoritmiem (EVERY_LOCAL_SEARCH_TYPE). Paneļa konfigurācija ir saglabāta BenchmarkConfig.xml failā. Piemērojot katru konfigurāciju dažāda mēroga piemēriem, novēroju, ka visi algoritmi sasniedz praktiski vienādu Best Score. Mērogošanas (Best Score scalability) ziņā mazliet labāks ir HILL_CLIMBING. Tā kā Best Score rādītāji ir līdzīgi, var salīdzināt Worst Score calculation speed differene. Šajā rādītājā VARIABLE_NEIGHBORHOOD_DESCENT, TABU_SEARCH un GREAT_DELUGE ir vājāki.
+
+Apskatīju kādi gājienu tipi (ChangeMove vai SwapMove) dod vislabāko izmaksu samazinājumu, taču atskaitē saņēmu atteikumu: "Graph not available. Either the statistic is not available for this solver configuration, or the benchmark failed."
+
 Risinājums ir mašīnai piekārtotas kravas kastes. Risinājuma novērtēšana tiek veikta, saskaitot kravas kastu kopējo izmēru katrā mašīnā. Jo mazāk mašīnās var satilpināt visas kastes, jo optimālāks risinājums. Izmaksu funkcija atgriež piepildītākās kravas mašīnas aizpildījumu (max). Par labāku risinājumu tiek uzskatīts arī vienmērīgāk sadalītas kastes starp mašīnām, respektīvi, mazāk "pārbāztas" mašīnas.
 Tālāk aprēķina izvēlētā risinājuma izmaksas un starpību starp līdz šim labākā risinājuma izmaksām (delta). Ja labāks par redzeto (izmaksu starpība (delta) ir negatīva) VAI metot kauliņu. Varbūtība akceptēt sliktāku risinājumu (random < exp) zemā temperatūrā samazinās. Varbūtība samazinās arī, ja liela izmaksu starpība (delta).
 Beigās pasludinām jaunatrasto risnājumu par līdz šim labāko risinājumu, ja tā izmaksas ir mazākas par iepriekšējo.
--- docbase.py
+++ docbase.py
@@ -1,16 +1,18 @@
+### Edgars Laķis
+### DatZ5057 : Datu apstrādes sistēmas
+### 3. mājas darbs (MongoDB skripts Python valodā, izmantojot PyMongo draiveri)
+
 import sys, requests, csv, pprint
 import pymongo
 from pymongo.mongo_client import MongoClient
 
+# Izveido jaunu klientu un savienojas ar MongoDB serveri
 uri = "mongodb+srv://edgarslakis:qXg26KFBoDnh5yZo@puduris0.93lsbgq.mongodb.net/?retryWrites=true&w=majority"
-
-# Create a new client and connect to the server
 client = MongoClient(uri)
-# use a database named "myDatabase"
+# Izveido šablona datubāzi "myDatabase" ar vienu kolekciju "proteins"
 db = client.myDatabase
-# use a collection named "recipes"
 my_collection = db["proteins"]
-# drop the collection in case it already exists
+# drop, ja kolekcija jau eksistē
 try:
   my_collection.drop()  
 # return a friendly error if an authentication error is thrown
@@ -19,7 +21,7 @@ except pymongo.errors.OperationFailure:
   sys.exit(1)
 
 def main():
-  # Send a ping to confirm a successful connection
+  # Ping
   try:
       client.admin.command('ping')
       print("Pinged your deployment. You successfully connected to MongoDB!")
@@ -29,33 +31,68 @@ def main():
   # Pievieno vienu dokumentu
   #mongoAdd(uniprot("P01583"))
 
-  # Pievieno 100 dokumentus no csv faila
+  # Izveido (doc_list funkcija) un pievieno (mongoAddMany funkcija) 100 dokumentus
   dokumenti = doc_list("acs.csv")
-  pprint.pprint(len(dokumenti))
   mongoAddMany(dokumenti)
-
-  query_one = {"primaryAccession": "P01583"}
-  # 1 atrast pēc unikāla vārda
-  result = my_collection.find_one(query_one)
-
+  print("############")
+  
+  # 1. atrast vienu dokumentu pēc viena ieraksta vērtības (primaryAccession) vai apakšvērtības (sequence[molWeight])
+  result = my_collection.find_one({"primaryAccession": "O60882"})
+  #result = my_collection.find_one({'sequence': {'molWeight': 35015}})
   if result is not None:
+    print("1. Atrasts dokuments pēc molWeight 35015")
     pprint.pprint(result)
+    print("############")
+  else:
+    print("Nav atrasts ieraksts")
+  
+  # 2. Atrast ierakstus ar mol_weight vērtību mazāk par 20'000 Da
+  results = my_collection.find({'sequence.molWeight': {'$lt': 20000}})
+  if results:
+    print("2. Ieraksti ar molWeight vērtību < 20 000")
+    for doc in results:
+      pprint.pprint(doc['sequence'])
+    print("############")
+  else:
+    print("Nav atrasti dokumenti")
+
+  # 3. Saskaitīt, cik dokumentu mol_weight vērtība ir lielāka par 50000
+  query_many = {'sequence.molWeight': {'$gt': 50000}}
+  results = my_collection.count_documents(query_many)
+  print("3. Dokumentu skaits ar molWeight > 50 000: ", results)
+  print("############")
+
+  # 4. izmaina mol_weight vērtību par 1000 Da
+  add_to_mass = {"$inc": {"sequence.molWeight": 1000}}
+  filter = {"primaryAccession": "O60882"}
+  update = my_collection.update_one(filter, add_to_mass)
+  # Pārbaudām, vai ir izmainīts ieraksts
+  result = my_collection.find_one({"primaryAccession": "O60882"})
+  if result is not None:
+    print("4. Izmainīta molWeight vērtība vienam dokumentam (primaryAccession: O60882)")
+    pprint.pprint(result)
+    print("############")
   else:
     print("Nav atrasts ieraksts")
 
-  query_many = {"sequence": {"molWeight": {"$gt": 10000}}}
-  # 2 mol_weight ir lielāks par 10'000 Da
-  results = my_collection.find(query_many)
+  # 5. Aprēķina vidējo mol_weight massu tiem ierakstiem mol_weight ir lielāks par 10'000 Da
+  pipeline = [{
+    "$group": {
+      "_id": None,
+      "videjais": {"$avg": "$sequence.molWeight"}      
+      }
+    }]
+  
+  result = list(my_collection.aggregate(pipeline))
+  if result:
+    print("5. Pipeline rezultāts: Vidējā molWeight vērtība: ", result[0]["videjais"])
+    print("############")
 
-  if results:
-    for column in results:
-      features = column['features']
-      features_count = len(column['features'])
-      mass = column['sequence']
-      pprint.pprint("Pretty print:", features_count, features)
-      print()      
-  else:
-    print("Nav atrasti dokumenti")
+  # 6. Izdzēst dokumentus ar O60882 VAI P08253
+  my_result = my_collection.delete_many({ "$or": [{"primaryAccession": "O60882"}, {"primaryAccession": "P08253"}]})
+  print("6. Izdzēsti dokumenti ar O60882 VAI P08253", my_result)
+  print("############")
+
 
 def doc_list(csvFile):
     ac = []
@@ -65,15 +102,10 @@ def doc_list(csvFile):
         for row in reader:
             dokuments = uniprot(row[2])
             ac.append(dokuments)
-    print("Trīs dokumenti", ac[5:7])
+    # print("Trīs dokumenti", ac[5:7])
     return ac
 
 def mongoAdd(entry):
-  # INSERT DOCUMENT
-  #
-  # You can insert individual documents using collection.insert_one().
-  # In this example, we're going to create four documents and then 
-  # insert them all with insert_many().
   try: 
     result = my_collection.insert_one(entry)
 
@@ -87,12 +119,6 @@ def mongoAdd(entry):
   print(result.inserted_id)
 
 def mongoAddMany(entryList):
-  # INSERT DOCUMENTS
-  #
-  # You can insert individual documents using collection.insert_one().
-  # In this example, we're going to create four documents and then 
-  # insert them all with insert_many().
-
   try: 
     result = my_collection.insert_many(entryList)
 
@@ -101,42 +127,8 @@ def mongoAddMany(entryList):
     print("An authentication error was received. Are you sure your database user is authorized to perform write operations?")
   else:
     inserted_count = len(result.inserted_ids)
-  print("ievietotais dokumentu skaits:", inserted_count)
+  print("Ievietoto dokumentu skaits:", inserted_count)
 
-def mongo(myDatabase):
-  
-
-  # UPDATE A DOCUMENT
-  #
-  # You can update a single document or multiple documents in a single call.
-  # 
-  # Here we update the prep_time value on the document we just found.
-  #
-  # Note the 'new=True' option: if omitted, find_one_and_update returns the
-  # original document instead of the updated one.
-
-  my_doc = my_collection.find_one_and_update({"ingredients": "potato"}, {"$set": { "prep_time": 72 }}, new=True)
-  if my_doc is not None:
-    print("Here's the updated recipe:")
-    print(my_doc)
-  else:
-    print("I didn't find any recipes that contain 'potato' as an ingredient.")
-  print("\n")
-
-  # DELETE DOCUMENTS
-  #
-  # As with other CRUD methods, you can delete a single document 
-  # or all documents that match a specified filter. To delete all 
-  # of the documents in a collection, pass an empty filter to 
-  # the delete_many() method. In this example, we'll delete two of 
-  # the recipes.
-  #
-  # The query filter passed to delete_many uses $or to look for documents
-  # in which the "name" field is either "elotes" or "fried rice".
-
-  my_result = my_collection.delete_many({ "$or": [{ "name": "elotes" }, { "name": "fried rice" }]})
-  print("I deleted %x records." %(my_result.deleted_count))
-  print("\n")
 
 def uniprot(accession):
     #accession = "P01583"
